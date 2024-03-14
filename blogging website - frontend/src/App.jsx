5e82cdbe68c43a97ef9c6c4723fd5c48144c440a
Suraj Vinod Miskin <Surajvmiskin@gmail.com>
2024-03-14 16:21:20 +0000

    start editing the files

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,7 +1,12 @@
+
+import Navbar from "./components/navbar.component";
+
 const App = () => {
     return (
-        <h1>MERN Blogging website by modern web</h1>
+        <Navbar />
     )
 }
 
 export default App;
+
+
