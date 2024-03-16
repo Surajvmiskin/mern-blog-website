ec58b2ac1c16b91537928c45984727a919e8175b
Suraj Vinod Miskin <Surajvmiskin@gmail.com>
2024-03-16 14:41:31 +0530

    completing the animation in Authpage

--- blogging website - frontend/src/common/page-animation.jsx
+++ blogging website - frontend/src/common/page-animation.jsx
@@ -2,14 +2,17 @@ import { AnimatePresence, motion } from 'framer-motion';
 
 const AnimationWrapper = ({ children, keyValue, initial = { opacity: 0 }, animate = { opacity: 1 }, transition = { duration: 1 } }) => {
     return (
-        <motion.div
-            key={keyValue}
-            initial={initial}
-            animate={animate}
-            transition={transition}
-        >
-            {children}
-        </motion.div>
+        <AnimatePresence>
+            <motion.div
+                key={keyValue}
+                initial={initial}
+                animate={animate}
+                transition={transition}
+                className={className}
+            >
+                {children}
+            </motion.div>
+        </AnimatePresence>
     )
 }
 
