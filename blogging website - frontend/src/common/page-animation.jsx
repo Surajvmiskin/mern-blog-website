f11ea0931758e8258b1a142778c105c4712e66d5
Suraj Vinod Miskin <Surajvmiskin@gmail.com>
2024-04-09 01:39:30 +0530

    Solving signup page errors

--- blogging website - frontend/src/common/page-animation.jsx
+++ blogging website - frontend/src/common/page-animation.jsx
@@ -1,6 +1,6 @@
 import { AnimatePresence, motion } from 'framer-motion';
 
-const AnimationWrapper = ({ children, keyValue, initial = { opacity: 0 }, animate = { opacity: 1 }, transition = { duration: 1 } }) => {
+const AnimationWrapper = ({ children, keyValue, initial = { opacity: 0 }, animate = { opacity: 1 }, transition = { duration: 1 }, className }) => {
     return (
         <AnimatePresence>
             <motion.div
